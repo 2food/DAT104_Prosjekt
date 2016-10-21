8609c6a7c3931dc5616cbcba0f4b53c0bf5ac054
Torstein <Torstein@Obviously>
2016-10-21 15:30:34 +0200

    database kontakt

---
+++ Sporris/src/no/hib/dat104/project/controller/UserTest.java
@@ -0,0 +1,36 @@
+package no.hib.dat104.project.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import no.hib.dat104.prosjekt.model.User;
+import no.hib.dat104.prosjekt.model.UserEAO;
+
+
+@WebServlet("/UserTest")
+public class UserTest extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	@EJB
+	private UserEAO ueao; 
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		PrintWriter out = response.getWriter();
+		List<User> l = ueao.allUsers();
+		for(User u : l) {
+			out.println(u.getUser_name() + "<br />");
+		}
+	}
+
+
+
+}
---
+++ Sporris/src/no/hib/dat104/prosjekt/model/UserEAO.java
@@ -0,0 +1,42 @@
+package no.hib.dat104.prosjekt.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Stateless
+public class UserEAO {
+
+	@PersistenceContext(name = "s_test")
+	private EntityManager em;
+
+
+	
+	public void addUser(User d) {
+		em.persist(d);
+	}
+
+	public List<User> allUsers() {
+		TypedQuery<User> query = em.createQuery("SELECT s FROM User s", User.class);
+		List<User> q = query.getResultList();
+		return q;
+	}
+	
+	public User findUser(int uid) {
+		return em.find(User.class, uid);
+	}
+	
+	public void updateUser(User s) {
+		em.merge(s);
+	}
+	
+	public void removeUser(int uid) {
+		em.remove(em.find(User.class, uid));
+	}
+	
+
+}
