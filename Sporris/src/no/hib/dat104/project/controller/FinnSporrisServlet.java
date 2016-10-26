667cc3acbbcf99058d914e77b896cd62cd6c2e5e
Vegardhh <vegardheitloholm@hotmail.com>
2016-10-26 16:19:39 +0200

    Opprettet sporrisEAO og FinnSporrisServlet

--- Sporris/src/no/hib/dat104/project/controller/FinnSporrisServlet.java
+++ Sporris/src/no/hib/dat104/project/controller/FinnSporrisServlet.java
@@ -1,8 +1,10 @@
 package no.hib.dat104.project.controller;
 
-import static no.hib.dat104.project.controller.UrlMappings.*;
+import static no.hib.dat104.project.controller.UrlMappings.FINNSPORRISURL;
+import static no.hib.dat104.project.controller.UrlMappings.SPORRISURL;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +12,38 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import no.hib.dat104.project.model.Sporris;
+import no.hib.dat104.project.model.SporrisEAO;
+
 @WebServlet("/" + FINNSPORRISURL )
 public class FinnSporrisServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+     
    
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.getRequestDispatcher("WEB-INF/jsp/finnSporris.jsp").forward(request, response);
 	}
 
+	/*
+	 * Sjekker om input tag er lik en tag som ligger i databasen for spørris. Hvis det finnes en match
+	 * så redirectes bruker til Sporris.jsp ettersom vi henviser til SporrisServlet
+	 * 
+	 * @Author Bojar
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	response.sendRedirect(SPORRISURL);
+		SporrisEAO sporrisEAO = new SporrisEAO();
+		List<Sporris> allSporris = sporrisEAO.allSporris();
+		String tag = request.getParameter("tag");
+		
+		
+		for(Sporris tagMatch : allSporris){
+			if(!tag.equals(tagMatch)){
+				response.sendRedirect(FINNSPORRISURL);
+			}else{
+				response.sendRedirect(SPORRISURL);
+			}
+		}
 	}
 
 }
---
+++ Sporris/src/no/hib/dat104/project/model/SporrisEAO.java
@@ -0,0 +1,46 @@
+package no.hib.dat104.project.model;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+@Stateless
+public class SporrisEAO {
+
+	
+	@PersistenceContext(name = "s_test")
+	private EntityManager em;
+
+
+	
+	public void addSporris(Sporris d) {
+		em.persist(d);
+	}
+
+	public List<Sporris> allSporris() {
+		TypedQuery<Sporris> query = em.createQuery("SELECT s FROM User s", Sporris.class);
+		List<Sporris> q = query.getResultList();
+		return q;
+	}
+	/*
+	 * Skal det være sid eller uid? Hvis det er feil så er det fordi det skal stå uid. 
+	 */
+	public Sporris findSporris(int sid) {
+		return em.find(Sporris.class, sid);
+	}
+	
+	public void updateUser(Sporris s) {
+		em.merge(s);
+	}
+	
+	/*
+	 * Samme potensielt idissue her. sid vs uid
+	 */
+	public void removeUser(int sid) {
+		em.remove(em.find(User.class, sid));
+	}
+	
+}
