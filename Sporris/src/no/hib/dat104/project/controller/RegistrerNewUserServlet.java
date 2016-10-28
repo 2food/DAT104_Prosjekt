1f0a9adf759dea6b60fabf0e48eca23b54601df7
Vegardhh <vegardheitloholm@hotmail.com>
2016-10-28 12:57:13 +0200

    registrer ny bruker servlet, jsp og javaBean

--- Sporris/src/no/hib/dat104/project/controller/RegistrerNewUserServlet.java
+++ Sporris/src/no/hib/dat104/project/controller/RegistrerNewUserServlet.java
@@ -1,25 +1,70 @@
 package no.hib.dat104.project.controller;
 
 import java.io.IOException;
+
+import javax.ejb.EJB;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static no.hib.dat104.project.controller.UrlMappings.*;
+import javax.servlet.http.HttpSession;
 
+import no.hib.dat104.project.helpers.SessionHelper;
+import no.hib.dat104.project.javabeans.LoginJavaBean;
+import no.hib.dat104.project.javabeans.RegistrerJavaBean;
+import no.hib.dat104.project.model.UserEAO;
+import no.hib.dat104.project.validators.RegisterValidator;
+
+import static no.hib.dat104.project.controller.UrlMappings.*;
 
 @WebServlet("/" + NEWUSERURL)
 public class RegistrerNewUserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	@EJB
+	UserEAO userEAO;
 
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession oldsession = request.getSession(false);
+		if (oldsession != null) {
+			oldsession.invalidate();
+		}
+		HttpSession session = request.getSession();
+		RegistrerJavaBean registrerInfo = new RegistrerJavaBean();
+		session.setAttribute("registrerInfo", registrerInfo);
 		request.getRequestDispatcher("WEB-INF/jsp/registerNewUser.jsp").forward(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect(OVERSIKTURL);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
+		String username = request.getParameter("username");
+		String userpw = request.getParameter("userpassword");
+		String userpw2 = request.getParameter("userepassword2");
+
+		RegistrerJavaBean registrerInfo = (RegistrerJavaBean) session.getAttribute("registrerInfo");
+		registrerInfo.setUsername(username);
+		registrerInfo.setUserpassword1(userpw);
+		registrerInfo.setUserpassword2(userpw2);
+		registrerInfo.setValidUsername(RegisterValidator.isValidUsername(username));
+		registrerInfo.setUsernameExists(RegisterValidator.usernameAlreadyExists(username, userEAO));
+		registrerInfo.setPasswordsMatches(RegisterValidator.passwordsmatches(userpw, userpw2));
+
+		//Logger inn og går til oversiktssiden hvis registreringsinfo er gyldig
+		//henter siden på nytt hvis inpyt ikke er gyldig og viser feilmeldinger
+		if (registrerInfo.isValidUsername() || !registrerInfo.isUsernameExists()
+				|| registrerInfo.isPasswordsMatches()) {
+			
+			//Legger til en login, true og username i session
+			SessionHelper.logInUser(request);
+			//Legg til bruker i databasen
+			response.sendRedirect(OVERSIKTURL);
+		} else {
+			response.sendRedirect(NEWUSERURL);
+		}
+
 	}
 
 }
--- Sporris/src/no/hib/dat104/project/validators/RegisterValidator.java
+++ Sporris/src/no/hib/dat104/project/validators/RegisterValidator.java
@@ -10,7 +10,7 @@ public class RegisterValidator {
 	 * Validerer brukernavn
 	 * @param username - Må ha mer enn 3 og mindre enn 20 tegn
 	 */
-	public boolean isValidUsername(String username){
+	public static boolean isValidUsername(String username){
 		boolean valid = true;
 		if (username.length() < 3 || username.length() > 20) {
 			valid = false;
@@ -22,7 +22,7 @@ public class RegisterValidator {
 	 * @param username, UserEAO
 	 * @return true hvis brukernavn finnes
 	 */
-	public boolean usernameAlreadyExists(String username, UserEAO UEAO){
+	public static boolean usernameAlreadyExists(String username, UserEAO UEAO){
 		// return UEAO.findUserByUserName(username)!= null
 		return false;
 	}
@@ -31,7 +31,7 @@ public class RegisterValidator {
 	 * @param pass1(passord), pass2(gjenta passord)
 	 * @return true hvis passord er like
 	 */
-	public boolean passwordsmatches(String pass1, String pass2){
+	public static boolean passwordsmatches(String pass1, String pass2){
 		return pass1.matches(pass2);
 	
 	}
