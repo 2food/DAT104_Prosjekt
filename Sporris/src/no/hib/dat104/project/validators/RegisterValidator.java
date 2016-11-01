b8875356d2eaaf0df6777387f55846430fb63388
Vegardhh <vegardheitloholm@hotmail.com>
2016-11-01 12:35:43 +0100

    registrer validator

--- Sporris/src/no/hib/dat104/project/validators/RegisterValidator.java
+++ Sporris/src/no/hib/dat104/project/validators/RegisterValidator.java
@@ -32,7 +32,7 @@ public class RegisterValidator {
 	 * @return true hvis passord er like
 	 */
 	public static boolean passwordsmatches(String pass1, String pass2){
-		return pass1.equals(pass2);
+		return pass1.matches(pass2);
 	
 	}
 }
